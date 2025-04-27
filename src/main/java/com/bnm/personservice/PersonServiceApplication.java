152e3eefa77259eb727cdaaef4f16b9f4cbe1d55
crazym8nd <undefined>
2025-04-27 19:03:16 +0300

    init

--- src/main/java/com/bnm/personservice/PersonServiceApplication.java
+++ src/main/java/com/bnm/personservice/PersonServiceApplication.java
@@ -2,12 +2,8 @@ package com.bnm.personservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaAuditing
-@EnableJpaRepositories(basePackages = "com.bnm.personservice.repository")
 public class PersonServiceApplication {
 
   public static void main(final String[] args) {
--- src/main/java/com/bnm/personservice/config/JpaAuditingConfig.java
+++ src/main/java/com/bnm/personservice/config/JpaAuditingConfig.java
@@ -4,13 +4,16 @@ import java.util.Optional;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "com.bnm.personservice.repository")
 @Configuration
 public class JpaAuditingConfig {
 
   @Bean
   public AuditorAware<String> auditorProvider() {
-    // В будущем здесь можно получать текущего пользователя из SecurityContext
     return () -> Optional.of("system");
   }
 } 
